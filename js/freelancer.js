22ee0e563d25c9ee64d6189cf38723ba047f8928
Richard Ludvigh <richard.ludvigh@gmail.com>
2014-05-25 20:48:14 +0200

    javascript scrolling fix

--- js/freelancer.js
+++ js/freelancer.js
@@ -3,7 +3,7 @@ $(function() {
     $('.page-scroll a').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
+            scrollTop: $($anchor.attr('href')).offset().top + 1
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
